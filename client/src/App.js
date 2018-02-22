6329cdcc6867f8d9ed323b1d34a82e0abe0ff863
Evgeniy Pivovarov <geny.pivo@gmail.com>
2018-02-22 15:21:20 +0200

    Basic front view

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Grid, Image, Button } from 'semantic-ui-react';
+import { Player } from 'video-react';
+import "../node_modules/video-react/dist/video-react.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Grid>
+          <Grid.Column width={10} className='video-player'>
+            <Player
+              playsInline
+              poster="/assets/poster.png"
+              src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+            />
+            <Grid className="video-buttons-group">
+              <Grid.Column width={5}>
+                <Button color="black" className="left floated">
+                  Video 1
+                </Button>
+              </Grid.Column>
+              <Grid.Column width={6}>
+                <Button color="black">
+                  Video 2
+                </Button>
+              </Grid.Column>
+              <Grid.Column width={5}>
+                <Button color="black" className="right floated">
+                  Video 3
+                </Button>
+              </Grid.Column>
+            </Grid>
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Grid>
+              <Grid.Row className="flex-center">
+                <Button color='red' className='control-button'>
+                  Start recording
+                </Button>
+              </Grid.Row>
+              <Grid.Column width={8}>
+
+              </Grid.Column>
+              <Grid.Column width={8}>
+
+              </Grid.Column>
+            </Grid>
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
